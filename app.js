2bb6f5b44f37be1f4f7d9d8a5f878b5075067764
Barbara Coronel Umbidez <barbara.coronel.umbidez@gmail.com>
2025-02-02 03:38:58 -0300

    creación de funciones

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById('amigo');
+    let nombre = input.value.trim();
+
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+
+    amigos.push(nombre);
+
+    let lista = document.getElementById('listaAmigos');
+    let nuevoAmigo = document.createElement('li');
+    nuevoAmigo.textContent = nombre;
+    lista.appendChild(nuevoAmigo);
+
+    input.value = '';
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No hay amigos para sortear.');
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    document.getElementById('resultado').innerHTML = `El amigo secreto es: <strong>${amigoSorteado}</strong>`;
+}
